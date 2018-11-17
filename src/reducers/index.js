941198d2995415738cb45fe9aa33764999df6601
samundrak <samundrak@yahoo.com>
2018-11-17 20:17:10 +0545

    First commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import apartmentsListReducer from './apartmentsListReducer';
+import apartmentReducer from './apartmentReducer';
+
+export default combineReducers({
+  apartmentsList: apartmentsListReducer,
+  apartmentItem: apartmentReducer,
+});
